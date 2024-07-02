6a1ada07cb1dc4ed49d492edbbc6319d4b983768
leonid1108 <luschnikov.lyonya@yandex.ru>
2024-07-02 13:39:20 +0400

    Initial commit to new repository

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -2,11 +2,12 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
-@Component
-public class Cat extends Animal {
+@Component()
+public class Dog extends Animal{
 
     @Override
     public String toString() {
-        return "Im a Cat";
+        return "I am Dog";
     }
+
 }
